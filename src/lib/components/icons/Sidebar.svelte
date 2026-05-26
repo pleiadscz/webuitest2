aaf566cf94013b2024c4de6acaa1f38972939c29
pleiadscz <pleiads@seznam.cz>
2026-05-26 04:45:52 +0200

    Update Sidebar.svelte

--- src/lib/components/icons/Sidebar.svelte
+++ src/lib/components/icons/Sidebar.svelte
@@ -6,22 +6,15 @@
 <svg
 	aria-hidden="true"
 	xmlns="http://www.w3.org/2000/svg"
-	fill="none"
 	viewBox="0 0 24 24"
-	stroke-width={strokeWidth}
-	stroke="currentColor"
+	fill="currentColor"
+	width="24"
+	height="24"
 	class={className}
-	><rect
-		x="3"
-		y="3"
-		width="18"
-		height="18"
-		rx="5"
-		ry="5"
-		stroke="currentColor"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		fill="none"
-	></rect><path d="M9.5 21V3" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-	></path></svg
+	stroke-width={strokeWidth}
 >
+	<path
+		d="M3 8C3 7.44772 3.44772 7 4 7H20C20.5523 7 21 7.44772 21 8C21 8.55228 20.5523 9 20 9H4C3.44772 9 3 8.55228 3 8ZM3 16C3 15.4477 3.44772 15 4 15H14C14.5523 15 15 15.4477 15 16C15 16.5523 14.5523 17 14 17H4C3.44772 17 3 16.5523 3 16Z"
+		fill="currentColor"
+	/>
+</svg>
